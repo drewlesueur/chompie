90f041eb3a876e99ba46db9599433ce86f5cafc5
drewlesueur <drewalex@gmail.com>
2012-11-07 08:15:07 +0000

    1 loop

--- ch2.js
+++ ch2.js
@@ -10,7 +10,14 @@ poorModule("chompie", function () { return function (chunks, scope) {
   , break_signal = "xyzzy"
   
   var handle_leaf_chunk = function () {
-    
+    if (starts_with(chunk, "'")) {
+      chunks[i] = chunk.substring(1)
+    } else {
+      
+    }
+  }, starts_with = function (str, sub){
+    if (!str) return false 
+    return str.substr(0,sub.length) == sub
   }, chunk_is_array = function(){ return _.isArray(chunk) 
   }, chunks_is_array = function(){ return _.isArray(chunks) 
   }, stack_chunks = function(){ chunks_stack.push(chunks)
@@ -23,6 +30,7 @@ poorModule("chompie", function () { return function (chunks, scope) {
       console.log("called", JSON.stringify(chunk))
     } else {
       console.log("get" + chunk)
+      handle_leaf_chunk()
     }
   }, step_into = function () { 
       stack_chunks()
@@ -30,27 +38,31 @@ poorModule("chompie", function () { return function (chunks, scope) {
       into_chunk()
       reset_i()
   }, step_outof = function () {
-      while (is_last_chunk()) {
-        unstack_chunks()
-        unstack_i()
-        update_chunk()
-        call()
-        inc_i()
-        if (chunks_stack_is_empty()) {
-          return break_signal
-        }
-      }
+    unstack_chunks()
+    unstack_i()
+    update_chunk()
+    call()
+    inc_i()
+    if (chunks_stack_is_empty()) {
+      return break_signal
+    }
+      
   }, handle_chunk = function () {
+    debugger
     if (chunk_is_array()) {
       step_into()
+    } else if (is_last_chunk()) {
+      return step_outof()
     } else {
-      handle_leaf_chunk()
       call()
       inc_i()
-      return step_outof()
+      //used to while loop and step out of
+      //but i only want 1 loop 
+      
     }
   }, update_chunk = function () { chunk = chunks[i]
   }, inc_i = function () { i += 1
+  }, chunk_is_null = function () { return chunk == null
   }, is_last_chunk = function () { return i == chunks.length
   }, chunks_stack_is_empty = function () { return chunks_stack.length == 0
   }, unstack_chunks = function () { chunks = chunks_stack.pop()
